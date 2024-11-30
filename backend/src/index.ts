8df1a7de6adab9a06d66f5c0078c64879fe21caa
samuelncaetano <samueln.caetano42@gmail.com>
2024-11-30 13:59:23 -0300

    feat(back): add the dependencies to create the server
    Refs #15

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
