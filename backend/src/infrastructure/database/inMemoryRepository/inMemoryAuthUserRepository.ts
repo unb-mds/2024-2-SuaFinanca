3a820a2264c816743ec49b440d57e42ce0a4e9af
samuelncaetano <samueln.caetano42@gmail.com>
2024-12-06 20:27:29 -0300

    feat(database): add the in-memory repository for user authentication
    Refs #20

---
+++ backend/src/infrastructure/database/inMemoryRepository/inMemoryAuthUserRepository.ts
@@ -0,0 +1,20 @@
+import {
+  CreateUserParams,
+  IAuthUserRepository,
+} from "@/application/interfaces/domain/entities/user/IauthUser";
+import { IUser, IUserWithId } from "@/domain/entities/user";
+
+export class InMemoryAuthUserRepository implements IAuthUserRepository {
+  private users: any[] = [];
+
+  async createUser(params: CreateUserParams): Promise<IUser> {
+    const newUser = { ...params };
+    this.users.push(newUser);
+    return newUser;
+  }
+
+  async findUserByEmail(email: string): Promise<IUserWithId | null> {
+    const user = this.users.find((user) => user.email === email);
+    return user || null;
+  }
+}
