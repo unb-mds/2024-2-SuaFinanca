f5e527fb80fe1c7af95a9eda856dc5327639aecb
samuelncaetano <samueln.caetano42@gmail.com>
2024-12-06 20:15:54 -0300

    feat(auth): add interfaces for user creation and login
    - Add IAuthUserRepository, CreateUserParams, and LoginUserParams interfaces in IauthUser.ts for user creation and login
    - Define CreateUserReturn, CreateUserResponse, LoginUserReturn, and LoginUserResponse interfaces in authUserProtocols.ts for consistent response structures
    - Add ICreateUserUseCase and ILoginUserUseCase interfaces in IuseCases.ts to define use case contracts for user creation and login
    
    Refs #20

---
+++ backend/src/application/interfaces/domain/entities/user/IauthUser.ts
@@ -0,0 +1,17 @@
+import { IUser, IUserWithId } from "@/domain/entities/user";
+
+export interface CreateUserParams {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface LoginUserParams {
+  email: string;
+  password: string;
+}
+
+export interface IAuthUserRepository {
+  createUser(params: CreateUserParams): Promise<IUser>;
+  findUserByEmail(email: string): Promise<IUserWithId | null>;
+}
