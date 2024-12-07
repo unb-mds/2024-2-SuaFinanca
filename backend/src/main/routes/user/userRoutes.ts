b6f7a1c89b52d88325607d2734cb96b85ce3870f
samuelncaetano <samueln.caetano42@gmail.com>
2024-12-07 17:05:33 -0300

    feat(routes): add routes for user login
    Refs #20

--- backend/src/main/routes/user/userRoutes.ts
+++ backend/src/main/routes/user/userRoutes.ts
@@ -1,8 +1,12 @@
-import { createUser } from "./userServiceRoutes";
+import { authUser, createUser, loginUser } from "./userServiceRoutes";
+
+import { authMiddleware } from "@/main/middlewares/authMiddleware";
 import express from "express";
 
 const userRoutes = express.Router();
 
 userRoutes.post("/user", createUser);
+userRoutes.post("/user/login", loginUser);
+userRoutes.get("/user/protected", authMiddleware, authUser);
 
 export default userRoutes;
--- backend/src/main/routes/user/userServiceRoutes.ts
+++ backend/src/main/routes/user/userServiceRoutes.ts
@@ -1,10 +1,25 @@
 import { Request, Response } from "express";
-
-import { createUserController } from "@/main/config/dependencyInjection/userDependencyInjection";
+import {
+  createUserController,
+  loginUserController,
+} from "@/main/config/dependencyInjection/userDependencyInjection";
 
 export async function createUser(req: Request, res: Response): Promise<void> {
   const { body, statusCode } = await createUserController.handle({
     body: req.body,
   });
   res.status(statusCode).send(body);
+}
+
+export async function loginUser(req: Request, res: Response): Promise<void> {
+  const { body, statusCode } = await loginUserController.handle({
+    body: req.body,
+  });
+  res.status(statusCode).send(body);
+}
+
+export async function authUser(req: Request, res: Response): Promise<void> {
+  res.json({
+    decodedToken: req.userId,
+  });
 }
