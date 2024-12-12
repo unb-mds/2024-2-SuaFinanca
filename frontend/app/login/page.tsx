41ec14ae40ab397a731d137d6ba87b21017ebd62
LucasMF1 <141961882+LucasMF1@users.noreply.github.com>
2024-12-12 20:03:25 -0300

    feat(frontend): user authentication (#28)
    * chore(frontend): add the dependencies frontend
    
    * feat(frontend): add the login
    
    * feat(frontend): add the register
    
    * feat(frontend): add the register
    
    * feat(login.tsx): interligar o login (front back)
    
    * feat(register.tsx): interligar o registro (front back)
    
    * feat(frontend): add the dashboard

---
+++ frontend/app/login/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { FaUser, FaLock } from "react-icons/fa";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import axios from "axios";
+import "./login.css";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post("http://localhost:8000/user/login", {
+        email: username,
+        password: password,
+      });
+
+      // Supondo que o back-end retorne um token e o nome do usuário
+      const { token, name } = response.data;
+
+      // Armazena o token e o nome do usuário no localStorage
+      localStorage.setItem("token", token);
+      localStorage.setItem("username", name);
+
+      alert("Login bem-sucedido!");
+      router.push("/dashboard");
+    } catch (err) {
+      setError("Credenciais inválidas!");
+    }
+  };
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        {error && <p className="error-message">{error}</p>}
+        <div className="input-field">
+          <input
+            type="email"
+            placeholder="E-mail"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <FaUser className="icon" />
+        </div>
+        <div className="input-field">
+          <input
+            type="password"
+            placeholder="Senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FaLock className="icon" />
+        </div>
+        <div className="recall-forget">
+          <label>
+            <input type="checkbox" /> lembre de mim
+          </label>
+          <a href="#">Esqueceu a senha?</a>
+        </div>
+        <button>Entrar</button>
+        <div className="signup-link">
+          <p>
+            Não tem uma conta? <a href="/register">Registrar</a>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
