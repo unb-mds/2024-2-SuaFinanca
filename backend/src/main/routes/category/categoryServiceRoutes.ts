f2e2fd5a46e3f1f5dc1282ae9f64321c2e1ce451
samuelncaetano <samueln.caetano42@gmail.com>
2025-01-13 19:15:38 -0300

    feat(routes): add the routes to create and get the categories
    Refs #33

---
+++ backend/src/main/routes/category/categoryServiceRoutes.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import {
+  createCategoryController,
+  getCategoriesByUserController,
+} from "@/main/config/dependencyInjection/categoryDependencyInjection";
+
+export async function createCategory(
+  req: Request,
+  res: Response,
+): Promise<void> {
+  const { body, statusCode } = await createCategoryController.handle({
+    body: { ...req.body, userId: req.userId },
+  });
+  res.status(statusCode).send(body);
+}
+
+export async function getCategoriesByUser(
+  req: Request,
+  res: Response,
+): Promise<void> {
+  const { body, statusCode } = await getCategoriesByUserController.handle({
+    userId: req.userId,
+  });
+  res.status(statusCode).send(body);
+}
