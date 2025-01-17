e2c48453c205d7abfc30c98911c24fdd5825bbce
samuelncaetano <samueln.caetano42@gmail.com>
2025-01-17 18:14:37 -0300

    feat(logs): add winston configuration file
    Refs #37

---
+++ backend/src/main/config/logs/log.ts
@@ -0,0 +1,20 @@
+import winston from "winston";
+
+const myFormat = winston.format.printf(
+  ({ level, message, label, timestamp }) => {
+    return `${timestamp} ${label} [${level}]: ${message}`;
+  },
+);
+
+export const log = (label: string) => {
+  return winston.createLogger({
+    level: "info",
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.label({ label: label }),
+      winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+      myFormat,
+    ),
+    transports: [new winston.transports.Console()],
+  });
+};
