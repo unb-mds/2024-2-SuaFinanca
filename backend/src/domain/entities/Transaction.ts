c0bd531f51283b44d6a22a811fc4645a292dc2fe
samuelncaetano <samueln.caetano42@gmail.com>
2025-01-19 10:02:58 -0300

    feat(entities): add financial transaction entity
    Refs #46

---
+++ backend/src/domain/entities/Transaction.ts
@@ -0,0 +1,21 @@
+export enum TransactionType {
+  INCOME = "INCOME",
+  EXPENSE = "EXPENSE",
+}
+
+export interface ITransaction {
+  type: TransactionType;
+  amount: number;
+  userId: number;
+  categoryId?: number;
+  date?: Date;
+}
+
+export interface ITransactionWithId {
+  id: number;
+  type: TransactionType;
+  amount: number;
+  userId: number;
+  categoryId?: number;
+  date?: Date;
+}
