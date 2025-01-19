432d921ec5993458f7a2749748de4396536660cf
samuelncaetano <samueln.caetano42@gmail.com>
2025-01-19 14:23:23 -0300

    feat(helpers): add the use case interface to delete a category
    Refs #53

--- backend/src/main/config/helpers/useCases/IuseCases.ts
+++ backend/src/main/config/helpers/useCases/IuseCases.ts
@@ -9,8 +9,8 @@ import {
 
 import { CreateCategoryParams } from "@/application/interfaces/domain/entities/category/IcategoryRepository";
 import { CreateCategoryReturn } from "@/main/config/helpers/protocol/category/createCategoryProtocols";
-import { GetCategoryByUserIdReturn } from "@/main/config/helpers/protocol/category/getCategoryProtocols";
 import { DeleteUserReturn } from "../protocol/user/deleteUserProtocols";
+import { GetCategoryByUserIdReturn } from "@/main/config/helpers/protocol/category/getCategoryProtocols";
 
 export interface ICreateUserUseCase {
   execute(params: CreateUserParams): Promise<CreateUserReturn | string>;
@@ -30,4 +30,8 @@ export interface IGetCategoriesByUserUseCase {
 
 export interface IDeleteUserUseCase {
   execute(id: number): Promise<DeleteUserReturn | string>;
+}
+
+export interface IDeleteCategoryUseCase {
+  execute(categoryId: number, userId: number): Promise<void | string>;
 }
