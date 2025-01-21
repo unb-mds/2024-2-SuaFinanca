1de29dec615864384d397c8ba9a7fa3ca8ff8303
samuelncaetano <samueln.caetano42@gmail.com>
2025-01-21 15:28:08 -0300

    feat(interfaces): add the methods in the repository interface
    Refs #54

--- backend/src/application/interfaces/domain/entities/category/IcategoryRepository.ts
+++ backend/src/application/interfaces/domain/entities/category/IcategoryRepository.ts
@@ -5,12 +5,23 @@ export interface CreateCategoryParams {
   userId: number;
 }
 
+export interface UpdateCategoryParams {
+  id: number;
+  name: string;
+  userId: number;
+}
+
 export interface ICategoryRepository {
   createCategory(category: CreateCategoryParams): Promise<ICategoryWithId>;
+  updateCategory(category: UpdateCategoryParams): Promise<ICategoryWithId>;
   findByName(name: string): Promise<ICategoryWithId | null>;
   findByUserId(userId: number): Promise<ICategoryWithId[]>;
   findByNameAndUserId(
     name: string,
+    userId: number,
+  ): Promise<ICategoryWithId | null>;
+  findByIdAndUserId(
+    id: number,
     userId: number,
   ): Promise<ICategoryWithId | null>;
 }
