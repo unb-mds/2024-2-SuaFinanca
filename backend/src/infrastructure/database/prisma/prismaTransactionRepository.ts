67eae244b256b3877dfa52b31a5f569068bfa5b1
samuelncaetano <samueln.caetano42@gmail.com>
2025-01-25 08:58:44 -0300

    feat(prisma): add the new methods of the Prisma Transaction Repository class
    Refs #75

--- backend/src/infrastructure/database/prisma/prismaTransactionRepository.ts
+++ backend/src/infrastructure/database/prisma/prismaTransactionRepository.ts
@@ -22,9 +22,49 @@ export class PrismaTransactionRepository implements ITransactionRepository {
         date: params.date,
       },
     });
+
+    const user = await prisma.user.findUnique({
+      where: { id: params.userId },
+    });
+
+    if (user) {
+      const newBalance =
+        params.type === TransactionType.INCOME
+          ? user.balance + params.amount
+          : user.balance - params.amount;
+
+      await prisma.user.update({
+        where: { id: params.userId },
+        data: { balance: newBalance },
+      });
+    }
+
     return {
       ...newTransaction,
       type: newTransaction.type as unknown as TransactionType,
     };
+  }
+
+  async findByUserIdAndMonthAndYearAndType(
+    userId: number,
+    month: number,
+    year: number,
+    type: TransactionType,
+  ): Promise<ITransactionWithId[]> {
+    const transactions = await prisma.transaction.findMany({
+      where: {
+        userId,
+        type,
+        date: {
+          gte: new Date(year, month, 1),
+          lt: new Date(year, month + 1, 1),
+        },
+      },
+    });
+
+    return transactions.map((transaction) => ({
+      ...transaction,
+      type: transaction.type as unknown as TransactionType,
+    }));
   }
 }
