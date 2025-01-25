3f81ef09719c9565024f44d53cbd6565ac27b455
Lucas Alves Oliveira dos Santos <155484556+LucasAlves71@users.noreply.github.com>
2025-01-25 18:50:22 -0300

    feat: Add user customization (#69)
    * refactor(iauthuser): refactor the IauthUser to do the User update
    
    * refactor(prismaauthuser): add updateUser on the prismaAuthUser
    
    * feat(userroutes): add userRoutes to update the user
    
    * feat(userserviceroutes): add updateUser on the User Service Routes
    
    * refactor(deleteusercontroller): add typeId and userId to improve the code
    
    * feat(iusecase): add the interface of IUpdateUserCase
    
    * feat(updateusercontroller): add user update controller
    
    * feat(updateuserschema): add user update schema
    
    * feat(updateusercase): add user update case
    
    * feat(updateuserprotocols): add the user update protocols
    
    * feat(user.http): add user update on the user.http
    
    * feat(updateuserusecase): add passwordHash to update User
    
    * feat(updateusercontroller): add 'error' to 'catch' and fix 'imports'
    
    * feat(userdependencyinjection): add updateUserUseCase and updateUserController
    
    * refactor(userserviceroutes): refact import and function deleteUser
    
    * refactor(userroutes): refact userRoutes.patch
    
    * feat(user.http): turn other 'delete' into 'patch' to complete the user's update
    
    * fix(src): fix imports and file names
    
    * fix: update linter command
    
    ---------
    
    Co-authored-by: samuelncaetano <samueln.caetano42@gmail.com>
    Co-authored-by: samuelncaetano <157507873+samuelncaetano@users.noreply.github.com>

---
+++ backend/src/application/useCases/updateUserUseCase.ts
@@ -0,0 +1,41 @@
+import {
+  UpdateUserParams,
+  IAuthUserRepository,
+} from "@/application/interfaces/domain/entities/user/IauthUser";
+import { IUpdateUserUseCase } from "@/main/config/helpers/useCases/IuseCases";
+import { UpdateUserReturn } from "@/main/config/helpers/protocol/user/updateUserProtocols";
+import { log } from "@/main/config/logs/log";
+import { PasswordHash } from "../interfaces/utils/passwordHash";
+
+const logger = log("UpdateUserUseCase");
+
+export class UpdateUserUseCase implements IUpdateUserUseCase {
+  constructor(
+    private readonly authUserRepository: IAuthUserRepository,
+    private readonly passwordHash: PasswordHash,
+  ) {}
+
+  async execute(params: UpdateUserParams): Promise<UpdateUserReturn | string> {
+    const user = await this.authUserRepository.findUserById(params.id);
+
+    if (!user) {
+      logger.warn(`User not found: ${params.id}`);
+      return "User not found";
+    }
+
+    if (params.password) {
+      const passwordHash = await this.passwordHash.hash(params.password);
+      params.password = passwordHash;
+    }
+
+    const updatedUser = await this.authUserRepository.updateUser(params);
+
+    logger.info(`User updated: ${updatedUser.email}`);
+    return {
+      user: {
+        name: updatedUser.name,
+        email: updatedUser.email,
+      },
+    };
+  }
+}
