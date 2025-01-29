1dac3555ab4c1ff1496022acad77e72f712b2db2
samuelncaetano <samueln.caetano42@gmail.com>
2025-01-29 17:41:00 -0300

    test(usecases): add tests for the use case that gets recent transactions
    Refs #93

---
+++ backend/test/transaction/useCases/getRecentTransactionsUseCase.spec.ts
@@ -0,0 +1,73 @@
+import { beforeEach, describe, expect, it } from "vitest";
+
+import { GetRecentTransactionsUseCase } from "@/application/useCases/transaction/getRecentTransactionsUseCase";
+import { InMemoryTransactionRepository } from "@/infrastructure/database/inMemoryRepository/inMemoryTransactionRepository";
+import { TransactionType } from "@/domain/entities/Transaction";
+
+describe("GetRecentTransactionsUseCase", () => {
+  let getRecentTransactionsUseCase: GetRecentTransactionsUseCase;
+  let inMemoryTransactionRepository: InMemoryTransactionRepository;
+
+  beforeEach(() => {
+    inMemoryTransactionRepository = new InMemoryTransactionRepository();
+    getRecentTransactionsUseCase = new GetRecentTransactionsUseCase(
+      inMemoryTransactionRepository,
+    );
+  });
+
+  it("should return recent transactions for a user", async () => {
+    // Arrange
+    const userId = 1;
+    const limit = 1;
+
+    await inMemoryTransactionRepository.createTransaction({
+      type: TransactionType.INCOME,
+      amount: 2000,
+      userId,
+      date: "2025-02-01T03:00:00Z",
+    });
+
+    await inMemoryTransactionRepository.createTransaction({
+      type: TransactionType.EXPENSE,
+      amount: 500,
+      userId,
+      date: "2025-02-10T03:00:00Z",
+    });
+
+    // Act
+    const result = await getRecentTransactionsUseCase.execute({
+      userId,
+      limit,
+    });
+
+    // Assert
+    expect(result).toEqual({
+      recent: {
+        transaction: [
+          {
+            id: 2,
+            userId: 1,
+            amount: 500,
+            date: new Date("2025-02-10T03:00:00.000Z"),
+            type: TransactionType.EXPENSE,
+          },
+        ],
+      },
+    });
+  });
+
+  it("should return an empty array if no transactions are found", async () => {
+    // Arrange
+    const userId = 1;
+    const limit = 2;
+
+    // Act
+    const result = await getRecentTransactionsUseCase.execute({
+      userId,
+      limit,
+    });
+
+    // Assert
+    expect(result.recent.transaction).toHaveLength(0);
+  });
+});
