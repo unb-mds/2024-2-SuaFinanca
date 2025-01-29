a9188f7e243dd0ee9712806d5be0d5ca02f4ce48
samuelncaetano <samueln.caetano42@gmail.com>
2025-01-29 16:28:53 -0300

    feat(config): add the dependency injections to get the transactions
    Refs #92

--- backend/src/main/config/dependencyInjection/transactionDependencyInjection.ts
+++ backend/src/main/config/dependencyInjection/transactionDependencyInjection.ts
@@ -2,6 +2,7 @@ import { CreateTransactionController } from "@/main/controllers/transaction/crea
 import { CreateTransactionUseCase } from "@/application/useCases/transaction/createTransactionUseCase";
 import { GetCategoryService } from "@/application/services/getCategoryService";
 import { GetUserBalanceController } from "@/main/controllers/transaction/getUserBalanceController";
+import { GetUserBalanceSummaryController } from "@/main/controllers/transaction/getUserBalanceSummaryController";
 import { GetUserBalanceUseCase } from "@/application/useCases/transaction/getUserBalanceUseCase";
 import { PrismaAuthUser } from "@/infrastructure/database/prisma/prismaAuthUser";
 import { PrismaCategoryRepository } from "@/infrastructure/database/prisma/prismaCategoryRepository";
@@ -30,4 +31,13 @@ const getUserBalanceController = new GetUserBalanceController(
   getUserBalanceUseCase,
 );
 
-export { createTransactionController, getUserBalanceController };
+// GetBalanceSummary
+const getUserBalanceSummaryController = new GetUserBalanceSummaryController(
+  getUserBalanceUseCase,
+);
+
+export {
+  createTransactionController,
+  getUserBalanceController,
+  getUserBalanceSummaryController,
+};
