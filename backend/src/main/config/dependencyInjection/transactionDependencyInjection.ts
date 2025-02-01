fdd70095ee2aa26c3eb4c5a9ba2401fc314a4160
Lucas Alves Oliveira dos Santos <lucasalves2005@gmail.com>
2025-02-01 16:43:07 -0300

    feat(transactiondependencyinjections): add delete transaction UseCase and Controller
    Refs #88

--- backend/src/main/config/dependencyInjection/transactionDependencyInjection.ts
+++ backend/src/main/config/dependencyInjection/transactionDependencyInjection.ts
@@ -8,6 +8,8 @@ import { PrismaCategoryRepository } from "@/infrastructure/database/prisma/prism
 import { PrismaTransactionRepository } from "@/infrastructure/database/prisma/prismaTransactionRepository";
 import { UpdateTransactionController } from "@/main/controllers/transaction/updateTransactionController";
 import { UpdateTransactionUseCase } from "@/application/useCases/transaction/updateTransactionUseCase";
+import { DeleteTransactionUseCase } from "@/application/useCases/transaction/deleteTransactionUseCase";
+import { DeleteTransactionController } from "@/main/controllers/transaction/deleteTransactionController";
 
 const prismaTransactionRepository = new PrismaTransactionRepository();
 const prismaCategoryRepository = new PrismaCategoryRepository();
@@ -41,8 +43,17 @@ const updateTransactionController = new UpdateTransactionController(
   updateTransactionUseCase,
 );
 
+// Delete
+const deleteTransactionUseCase = new DeleteTransactionUseCase(
+  prismaTransactionRepository,
+);
+const deleteTransactionController = new DeleteTransactionController(
+  deleteTransactionUseCase,
+);
+
 export {
   createTransactionController,
   getUserBalanceController,
   updateTransactionController,
+  deleteTransactionController,
 };
