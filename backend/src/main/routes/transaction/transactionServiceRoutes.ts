4d28167bacfc03ceb5bd89870f6fede647a9e213
Lucas Alves Oliveira dos Santos <lucasalves2005@gmail.com>
2025-02-01 16:38:50 -0300

    feat(transactionserviceroutes): add delete transaction function
    Refs #88

--- backend/src/main/routes/transaction/transactionServiceRoutes.ts
+++ backend/src/main/routes/transaction/transactionServiceRoutes.ts
@@ -3,6 +3,7 @@ import {
   createTransactionController,
   getUserBalanceController,
   updateTransactionController,
+  deleteTransactionController,
 } from "@/main/config/dependencyInjection/transactionDependencyInjection";
 
 export async function createTransaction(
@@ -34,6 +35,17 @@ export async function updateTransaction(
     params: req.params,
     userId: req.userId,
     body: req.body,
+  });
+  res.status(statusCode).send(body);
+}
+
+export async function deleteTransaction(
+  req: Request,
+  res: Response,
+): Promise<void> {
+  const { body, statusCode } = await deleteTransactionController.handle({
+    params: req.params,
+    userId: req.userId,
   });
   res.status(statusCode).send(body);
 }
