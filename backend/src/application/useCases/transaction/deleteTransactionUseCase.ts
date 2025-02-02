f061e15d811108b396e57c5367fd1f66c5d0e4ce
samuelncaetano <samueln.caetano42@gmail.com>
2025-02-02 19:17:34 -0300

    refactor(usecases): change the logic used to delete transactions in the use case
    Refs #88

--- backend/src/application/useCases/transaction/deleteTransactionUseCase.ts
+++ backend/src/application/useCases/transaction/deleteTransactionUseCase.ts
@@ -1,10 +1,18 @@
-import { ITransactionRepository } from "@/application/interfaces/domain/entities/transaction/ItransactionRepository";
+import { DeleteTransactionReturn } from "@/main/config/helpers/protocol/transaction/deleteTransactionProtocols";
 import { IDeleteTransactionUseCase } from "@/main/config/helpers/useCases/IuseCases";
+import { IGetCategoryService } from "@/application/interfaces/services/IgetCategoryService";
+import { ITransactionRepository } from "@/application/interfaces/domain/entities/transaction/ItransactionRepository";
 
 export class DeleteTransactionUseCase implements IDeleteTransactionUseCase {
-  constructor(private readonly transactionRepository: ITransactionRepository) {}
+  constructor(
+    private readonly transactionRepository: ITransactionRepository,
+    private readonly categoryService: IGetCategoryService,
+  ) {}
 
-  async execute(transactionId: number, userId: number): Promise<string | void> {
+  async execute(
+    transactionId: number,
+    userId: number,
+  ): Promise<DeleteTransactionReturn | string> {
     const transaction = await this.transactionRepository.findByIdAndUserId(
       transactionId,
       userId,
@@ -15,5 +23,29 @@ export class DeleteTransactionUseCase implements IDeleteTransactionUseCase {
     }
 
     await this.transactionRepository.deleteTransaction(transactionId, userId);
+
+    let categoryName: string | undefined;
+
+    if (transaction.categoryId) {
+      const category = await this.categoryService.getCategoryByIdAndUserId(
+        transaction.categoryId,
+        userId,
+      );
+
+      if (!category) {
+        return "Category not found";
+      }
+
+      categoryName = category.name;
+    }
+
+    return {
+      transaction: {
+        type: transaction.type,
+        amount: transaction.amount,
+        categoryName: categoryName ?? undefined,
+        date: transaction.date,
+      },
+    };
   }
 }
